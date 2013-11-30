a69cd86930130d4e6ea04573ce54cfa11fec7422
eltonsantos <eltonaxl@hotmail.com>
2013-11-30 16:32:58 -0300

    Initial repository, configs and instalation gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InitialRails::Application.config.secret_key_base = '1dc0e1ebea840ed3cc9d39bc18e3a6f8167e9535cbec64d6c061ca0d6f868389d8564e2cf6c6993e2939d6d6f07154174f325c94245fadb8f3083c92e55c210c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InitialRails::Application.config.session_store :cookie_store, key: '_initial_rails_session'
